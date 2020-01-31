8d510c3dfb791698d07cdf8501395d17cac37fcf
UbuntuHome <RagdollNoone@163.com>
2020-01-31 14:13:35 +0800

    add new_topic page

--- Django/first_project/learning_logs/views.py
+++ Django/first_project/learning_logs/views.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from .models import Topic, Entry
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import TopicForm
 
 
 # Create your views here.
@@ -20,3 +23,16 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
